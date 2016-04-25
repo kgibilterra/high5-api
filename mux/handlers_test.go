fadce82551f841565c45ded28210fa1e03a25629
Kaylyn Gibilterra <kgibilterra@gmail.com>
2016-04-24 20:25:17 -0400

    fixed test calls to point to handlers.go

--- mux/handlers_test.go
+++ mux/handlers_test.go
@@ -1,8 +1,6 @@
 package mux
 
 import (
-	"github.com/gorilla/mux"
-
 	"fmt"
 	"io"
 	"net/http"
@@ -18,24 +16,39 @@ var (
 )
 
 func init() {
-	router := mux.NewRouter()
+	router := NewRouter()
 	server = httptest.NewServer(router)
 	server.URL = "http://localhost:8787"
-	highFiveUrl = fmt.Sprintf("%s/highFive", server.URL)
+	defer server.Close()
 }
 
 func TestPostHighFive(t *testing.T) {
-	highFive := `{"sender": "Dennis", "receiver": "Kaylyn", "message": "Thanks"}`
-
+	highFive := `{"sender": "Dennis", "receiver": "Kaylyn2", "message": "Thanks"}`
+	highFiveUrl = fmt.Sprintf("%s/highFive", server.URL)
 	reader = strings.NewReader(highFive)
 	request, err := http.NewRequest("POST", highFiveUrl, reader)
-	res, err := http.DefaultClient.Do(request)
-
 	if err != nil {
 		t.Error(err)
 	}
 
-	if res.StatusCode != 201 {
-		t.Error("Success expected: %d", res.StatusCode)
+	w := httptest.NewRecorder()
+	PostHighFive(w, request)
+	if w.Code != 201 {
+		t.Error("Success expected: %d", w.Code)
+	}
+}
+
+func TestGetHighFives(t *testing.T) {
+	highFiveUrl = fmt.Sprintf("%s/highFives", server.URL)
+	reader = strings.NewReader("")
+	request, err := http.NewRequest("GET", highFiveUrl, reader)
+	if err != nil {
+		t.Error(err)
+	}
+
+	w := httptest.NewRecorder()
+	GetHighFives(w, request)
+	if w.Code != 200 {
+		t.Error("Success expected: %d", w.Code)
 	}
 }
